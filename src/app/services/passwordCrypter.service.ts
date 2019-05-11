045f91f280b0bf383b7af409191555eab21a2e92
Henry Muessemann <h.muessemann@gmx.de>
2019-05-11 18:44:33 +0200

    minor bug fixes

--- src/app/services/passwordCrypter.service.ts
+++ src/app/services/passwordCrypter.service.ts
@@ -15,16 +15,14 @@ export class PasswordCrypterService {
 
     getSHA512(s :string) :string
     {
-        var SHA512 = require("crypto-js/sha512");
-        var b64 = SHA512(s);
+        var b64 = crypto.sha512(s);
         var eHex = b64.toString();
         return eHex;
     }
 
     getSHA256(s :string) :string
     {
-        var SHA256 = require("crypto-js/sha256");
-        var b64 = SHA256(s);
+        var b64 = crypto.sha256(s);
         var out = b64.toString();
         return out;
     }
@@ -36,8 +34,7 @@ export class PasswordCrypterService {
 
         var shareText = JSON.stringify(share);
 
-        var AES = require("crypto-js/aes");
-        var b64 = AES.encrypt(shareText, SHA256EncryptedPW);
+        var b64 = crypto.aes.encrypt(shareText, SHA256EncryptedPW);
 
         let hexString = b64.toString();
         return hexString;
@@ -46,10 +43,8 @@ export class PasswordCrypterService {
     decryptPasswordShare(encryptedText :string, pw :string) : {}
     {
         var SHA256EncryptedPW = this.getSHA256(pw);
-        
-        var AES = require("crypto-js/aes");
 
-        var decrypted = AES.decrypt(encryptedText, SHA256EncryptedPW);
+        var decrypted = crypto.aes.decrypt(encryptedText, SHA256EncryptedPW);
         var plaintext = decrypted.toString(crypto.enc.Utf8);
 
         return plaintext
